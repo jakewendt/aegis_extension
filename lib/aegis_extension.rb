545552f0abd32ba688721fb4ffd5e19dd2b7affa
Jake <github@jake.otherinbox.com>
2010-02-22 11:57:04 -0800

    Added Permissions methods

--- lib/aegis_extension.rb
+++ lib/aegis_extension.rb
@@ -1,2 +1,3 @@
 # AegisExtension
 require 'aegis_extension/has_role'
+require 'aegis_extension/permissions'
--- rails/init.rb
+++ rails/init.rb
@@ -6,3 +6,5 @@ ActiveRecord::Base.class_eval do
 	#	override validates_role_name
   extend AegisExtension::HasRole
 end
+Aegis::Permissions.send(:include, AegisExtension::Permissions )
+
